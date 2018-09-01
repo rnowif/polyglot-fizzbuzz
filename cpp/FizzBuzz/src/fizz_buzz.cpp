0e604e628c00a9a3cf0689a677b50963e9342bba
Renaud Humbert-Labeaumaz <rnow.if@gmail.com>
2018-09-01 17:08:55 +0200

    cpp(*): refactor to have declarative rules

--- cpp/FizzBuzz/src/fizz_buzz.cpp
+++ cpp/FizzBuzz/src/fizz_buzz.cpp
@@ -1,22 +1,24 @@
 #include <string>
+#include <vector>
 
 using namespace std;
+
+vector<pair<int, string>> rules {
+        {15, "FizzBuzz"},
+        {3, "Fizz"},
+        {5, "Buzz"}
+};
 
 bool isMultipleOf(int multiple, int number) {
     return number % multiple == 0;
 }
 
 string FizzBuzz(int number) {
-    if (isMultipleOf(15, number)) {
-        return "FizzBuzz";
-    }
 
-    if (isMultipleOf(3, number)) {
-        return "Fizz";
-    }
-
-    if (isMultipleOf(5, number)) {
-        return "Buzz";
+    for (auto rule : rules) {
+        if (isMultipleOf(rule.first, number)) {
+            return rule.second;
+        }
     }
 
     return to_string(number);
