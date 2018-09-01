e9286f828cc235755ec9d35e5ff35141aeb90645
Renaud Humbert-Labeaumaz <rnow.if@gmail.com>
2018-09-01 16:17:07 +0200

    cpp(*): should return Fizz when multiple of 3

--- cpp/FizzBuzz/src/fizz_buzz.cpp
+++ cpp/FizzBuzz/src/fizz_buzz.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 string FizzBuzz(int number) {
+    if (number % 3 == 0) {
+        return "Fizz";
+    }
+
     return to_string(number);
 }
 
--- cpp/FizzBuzz/tests/unit_tests/test_fizz_buzz.cpp
+++ cpp/FizzBuzz/tests/unit_tests/test_fizz_buzz.cpp
@@ -8,3 +8,11 @@ TEST(FizzBuzz, should_return_one_when_one) {
 TEST(FizzBuzz, should_return_n_when_n) {
     EXPECT_EQ("2", FizzBuzz(2));
 }
+
+TEST(FizzBuzz, should_return_fizz_when_3) {
+    EXPECT_EQ("Fizz", FizzBuzz(3));
+}
+
+TEST(FizzBuzz, should_return_fizz_when_multiple_of_3) {
+    EXPECT_EQ("Fizz", FizzBuzz(6));
+}
